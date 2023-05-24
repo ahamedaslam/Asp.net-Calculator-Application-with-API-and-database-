b44f9fd2c2d6b17c66b1af726a22ef1918760201
Ahamed Aslam <87639294+ahamedaslam@users.noreply.github.com>
2023-05-24 18:26:52 +0530

    Add files via upload
    Model of calculator UI

---
+++ Calc.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calc_ui.Models
+{
+    public class Calc
+    {
+        [Key]
+        public int Value1 { get; set; }
+   
+        public int Value2 { get; set; }
+
+        public string Operand { get; set; }
+        public int Total { get; set; }
+
+        public string Calculate { get; set; }
+    }
+}
